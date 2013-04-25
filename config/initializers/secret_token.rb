0636a90ac2c47e2f0e341c9baec96f39a6a03aef
Varela <guilhermevarela@hotmail.com>
2013-04-25 12:10:52 -0300

    Revert "Initial commit"
    This reverts commit 411bf4c7dfeaaa5f5b895b3780019b84b6356d15.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '4175063eb099b90bdde80cb472fb814053e8390328efc67f1eec742f372398079f76ce47a17a44f56fd756022f4f60c4f15d7ab3d7f3d680d051dc322efa4ac5'
